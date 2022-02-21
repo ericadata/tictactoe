6cf7f67c8a2397ed0feb187170cd180d469861b8
Erica Moses <ericamoses84@gmail.com>
2022-02-21 09:39:07 -0800

    delete choice of X vs O

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,17 @@
 from random import choice
 
+#CHANGE BOARD TO STRING THINGY THAT CAN BE PRINTED OUT
 board = [["","",""],["","",""],["","",""]]
 locations = [1,2,3,4,5,6,7,8,9]
 counter = 0
 
+#FILL THIS OUT
 def display_board():
     pass
 
 def virtual_play():
     global counter
-    if letter_selection == "X":
-        virtual_letter = "O"
-    virtual_letter = "X"
+    letter_selection = "O"
     location = choice(locations)
     print("The location {location} was taken!".format(location=location))
     counter += 1
@@ -20,6 +20,7 @@ def virtual_play():
 
 def player_play():
     global counter
+    letter_selection = "X"
     location = input("Where do you want the {letter_selection} to go? ".format(letter_selection=letter_selection))
     location = int(location)
     #print(location in locations)
@@ -32,13 +33,6 @@ def player_play():
     locations.remove(location)
 
 print("Welcome to a game of tic-tac-toe!")
-letters = ["X", "O", "x", "o"]
-letter_selection = ""
-letter_selection = input("Would you like to be X or O? ")
-while letter_selection not in letters:
-    print("Please choose an X or O.")
-    letter_selection = input("Would you like to be X or O? ")
-    print(letter_selection)
 
 print("Let the game begin!")
 print("""
@@ -49,11 +43,10 @@ ________________     7 | 8 | 9
 TO PLAY TIC - TAC - TOE, YOU NEED TO GET THREE IN A ROW.
 YOUR CHOICES ARE BETWEEN 1 TO 9.
 """)
+#FIGURE OUT REVERSE LOGIC, WHAT IF THE OTHER PERSON IS O? OR JUST MAKE IT EASIER AND GET RID THE CHOICE
 while counter < 5:
-    if letter_selection.upper() == "X":
-        player_play()
-    else:
-        virtual_play()
+    player_play()
+    virtual_play()
     print("Now the computer shall chose a spot!")
     virtual_play()
 
