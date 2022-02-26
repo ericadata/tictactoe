dd6a528d7c07b03bafcf30ed134e78d96ada522b
Erica Moses <ericamoses84@gmail.com>
2022-02-26 09:56:37 -0800

    fix end message

--- tictactoe.py
+++ tictactoe.py
@@ -6,19 +6,15 @@ board = """
     4 | 5 | 6
     7 | 8 | 9
     """
-#[["","",""],["","",""],["","",""]]
 locations = [1,2,3,4,5,6,7,8,9]
 counter = 0
 
-#FILL THIS OUT
-def display_board():
-    print(board)
-
 def virtual_play():
     global counter
+    global board
     letter_selection = "O"
     location = choice(locations)
-    board.replace(str(location), letter_selection)
+    board = board.replace(str(location), letter_selection)
     print("The location {location} was taken!".format(location=location))
     counter += 1
     print(counter)
@@ -26,14 +22,17 @@ def virtual_play():
 
 def player_play():
     global counter
+    global board
     letter_selection = "X"
     location = input("Where do you want the X to go? ")
     location = int(location)
+    board = board.replace(str(location), letter_selection)
     #print(location in locations)
     while location not in locations:
         print("That location is taken! Pick again.") 
         location = input("Where do you want the X to go? ")
         location = int(location)
+        board = board.replace(str(location), letter_selection)
     counter += 1
     print(counter)
     locations.remove(location)
@@ -51,10 +50,10 @@ YOUR CHOICES ARE BETWEEN 1 TO 9.
 """)
 while counter < 5:
     player_play()
-    display_board()
+    print(board)
     print("Now the computer shall chose a spot!")
     virtual_play()
-    display_board()
+    print(board)
 
 positive = ["y", "Y", "Yes", "yes", "YES"]
 negative = ["n", "N", "no", "NO", "No"]
@@ -63,7 +62,8 @@ if end_game in positive:
     result = input("Did you win? y/n ")
     if result in positive:
         print("You won!")
-    print("Better luck next time!")
+    else:
+        print("Better luck next time!")
 else:
     while end_game not in positive:
         player_play()
