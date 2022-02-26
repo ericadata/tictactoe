814253610657af6e1e4b1ac32de1eea44bd0939a
Erica Moses <ericamoses84@gmail.com>
2022-02-26 09:08:05 -0800

    change the board

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,24 @@
 from random import choice
 
 #CHANGE BOARD TO STRING THINGY THAT CAN BE PRINTED OUT
-board = [["","",""],["","",""],["","",""]]
+board = """
+    1 | 2 | 3
+    4 | 5 | 6
+    7 | 8 | 9
+    """
+#[["","",""],["","",""],["","",""]]
 locations = [1,2,3,4,5,6,7,8,9]
 counter = 0
 
 #FILL THIS OUT
 def display_board():
-    pass
+    print(board)
 
 def virtual_play():
     global counter
     letter_selection = "O"
     location = choice(locations)
+    board.replace(str(location), letter_selection)
     print("The location {location} was taken!".format(location=location))
     counter += 1
     print(counter)
@@ -21,7 +27,7 @@ def virtual_play():
 def player_play():
     global counter
     letter_selection = "X"
-    location = input("Where do you want the {letter_selection} to go? ".format(letter_selection=letter_selection))
+    location = input("Where do you want the X to go? ")
     location = int(location)
     #print(location in locations)
     while location not in locations:
@@ -43,10 +49,10 @@ ________________     7 | 8 | 9
 TO PLAY TIC - TAC - TOE, YOU NEED TO GET THREE IN A ROW.
 YOUR CHOICES ARE BETWEEN 1 TO 9.
 """)
-#FIGURE OUT REVERSE LOGIC, WHAT IF THE OTHER PERSON IS O? OR JUST MAKE IT EASIER AND GET RID THE CHOICE
 while counter < 5:
     player_play()
-    virtual_play()
+    display_board()
     print("Now the computer shall chose a spot!")
     virtual_play()
+    display_board()
 
